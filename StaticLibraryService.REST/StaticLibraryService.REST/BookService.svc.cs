5f122789737040966a4b3e2df2c3cf2d0bfd63c9
ba138360 <ba138360@UKST0055>
2019-04-25 10:49:46 +0100

    removed json object serialization to string such that methods interface with Book objet directly

--- StaticLibraryService.REST/StaticLibraryService.REST/BookService.svc.cs
+++ StaticLibraryService.REST/StaticLibraryService.REST/BookService.svc.cs
@@ -14,28 +14,24 @@ namespace StaticLibraryService.REST
     [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
     public class BookService
     {
-        private static List<Book> mCatalogue = new List<Book>() { new Book("Jane Eyre"), new Book("Robinson Crusoe"), new Book("Macbeth")};
+        private static List<Book> mCatalogue = new List<Book>() { new Book { Title = "Jane Eyre" }, new Book { Title = "Robinson Crusoe" }, new Book { Title = "Macbeth" } };
 
-        [WebGet(UriTemplate="/Catalogue", ResponseFormat =WebMessageFormat.Xml)]
+        [WebGet(UriTemplate="Catalogue", ResponseFormat = WebMessageFormat.Json)]
         [OperationContract]
-        public string GetCatalogue()
+        public Book[] GetCatalogue()
         {
-            string catalogue = "";
-            if (mCatalogue.Count > 0)
-                catalogue = JsonConvert.SerializeObject(mCatalogue, Formatting.Indented);
-            return catalogue;
+            return mCatalogue.ToArray();
         }
 
         [WebGet(UriTemplate = "/Catalogue/{catalogueIndex}", ResponseFormat = WebMessageFormat.Json)]
         [OperationContract]
-        public string GetBook(string catalogueIndex)
+        public Book GetBook(string catalogueIndex)
         {
-            string catalogue = "";
             uint index;
             uint.TryParse(catalogueIndex, out index);
             if (mCatalogue.Count > 0 && index < mCatalogue.Count)
-                catalogue = JsonConvert.SerializeObject(mCatalogue[(int)index], Formatting.Indented);
-            return catalogue;
+                return mCatalogue[(int)index];
+            return new Book { };
         }
 
         [WebInvoke(Method = "DELETE", UriTemplate = "/Catalogue/{catalogueIndex}")]
@@ -59,26 +55,24 @@ namespace StaticLibraryService.REST
 
         [WebInvoke(Method = "PUT", UriTemplate = "/Catalogue/{catalogueIndex}", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
         [OperationContract]
-        public void UpdateBook(string bookAsString, string catalogueIndex)
+        public void UpdateBook(Book updatedBook, string catalogueIndex)
         {
             uint index;
             uint.TryParse(catalogueIndex, out index);
-            Book updatedBook = JsonConvert.DeserializeObject<Book>(bookAsString);
             bool validUpdate = (mCatalogue.Count > 0 && index < mCatalogue.Count && updatedBook.Title != null && updatedBook.Title != "");
             if (validUpdate)
                 mCatalogue[(int)index] = updatedBook;
         }
     }
 
-    class Book
+    [DataContract]
+    public class Book
     {
-        public Book(string title)
-        {
-            Title = title;
-        }
-
+        [DataMember]
         public string Title { get; set; }
+        [DataMember]
         public string Author { get; set; }
+        [DataMember]
         public string Publisher { get; set; }
     }
 }
